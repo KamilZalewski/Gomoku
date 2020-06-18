c3b574f230c6a460f71bac59d2c45550b0a93753
KamilZalewski <kamilzalewski89@gmail.com>
2020-06-18 14:12:28 +0200

    Gomoku next version

--- src/main/java/com/kodilla/game/logic/Game.java
+++ src/main/java/com/kodilla/game/logic/Game.java
@@ -1,8 +1,15 @@
 package com.kodilla.game.logic;
 
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class Game {
     private final GridPane grid;
@@ -32,8 +39,28 @@ public class Game {
             }
             if (isWinner(PlayerColor.WHITE))
                 System.out.println("White is a winner.");
+                Stage window = new Stage();
+                window.initModality(Modality.APPLICATION_MODAL);
+                window.setTitle("Game Over!");
+                window.setMinWidth(250);
+                Label label = new Label();
+                label.setText(PlayerColor.WHITE + " is a Winner!\n Congratulations!");
+                Button newGameButton = new Button("New Game");
+                newGameButton.setOnAction(e ->
+                        window.close());
+                Button endGameButton = new Button("End Game");
+                endGameButton.setOnAction(e ->
+                        window.close());
+                VBox layout = new VBox(10);
+                layout.getChildren().addAll(label, newGameButton, endGameButton);
+                layout.setAlignment(Pos.CENTER);
+                Scene scene = new Scene(layout);
+                window.setScene(scene);
+                window.show();
             if (isWinner(PlayerColor.BLACK))
                 System.out.println("Black is a winner.");
+
+
         }
     }
 
@@ -60,7 +87,9 @@ public class Game {
         result = result || checkDown(playerColor, col, row);
         result = result || checkRightDiagonal(playerColor, col, row);
         result = result || checkLeftDiagonal(playerColor, col, row);
+
         return result;
+
     }
 
     private boolean checkLeftDiagonal(PlayerColor playerColor, int col, int row) {
@@ -104,8 +133,6 @@ public class Game {
     }
 
     public void display() {
-        board.setFigure(3,3,new Pawn(PlayerColor.WHITE));
-        board.setFigure(7,7,new Pawn(PlayerColor.BLACK));
         grid.getChildren().clear();
         for(int col = 0; col < 15; col++){
             for(int row = 0; row < 15; row++){
